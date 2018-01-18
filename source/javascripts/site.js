559d0bb6d7c874090d54f45128dbdc3182f589c9
Diana <diana.didijtsuk@gmail.com>
2018-01-18 21:44:32 +0200

    Update

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,5 @@
+/* Smooth scroll */
+
 $(document).ready(function(){
 	$('.smooth-scroll').click(function() {
 		$('html, body').animate({
@@ -5,3 +7,27 @@ $(document).ready(function(){
 		}, 1000);
 	});
 });
+
+/* Mobile navigation button toggle */
+
+ $(document).ready(function(){
+	$('#hideshow').on('click', function(event) {        
+		 $('#navbar-mobile').toggle('show');
+	});
+});
+
+/* Background scroll */
+
+var velocity = 0.3;
+
+function update(){ 
+var pos = $(window).scrollTop(); 
+$('#forwhom').each(function() { 
+    var $element = $(this);
+    var height = $element.height()+135;
+    $(this).css('backgroundPosition', '20% ' + Math.round((height - pos) * velocity) +  'px'); 
+   }); 
+   };
+
+ $(window).bind('scroll', update);
+    
