5f34f08b746bea7af3cb36d64ea9230709f8f0d2
Diana <33287508+VeronaDi@users.noreply.github.com>
2018-01-21 23:27:36 +0200

    Update

---
+++ build/javascripts/site.js
@@ -0,0 +1,25 @@
+/* Smooth scroll */
+
+$(document).ready(function(){
+	$('.smooth-scroll').click(function() {
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+		}, 1000);
+	});
+});
+
+/* Mobile navigation button toggle */
+
+ $(document).ready(function(){
+	$('#hideshow').on('click', function(event) {        
+		 $('#navbar-mobile').toggle('show');
+	});
+});
+
+/* Background scroll */
+$(document).ready(function(){
+$(window).scroll(function () {
+    $(".bckg-image").css("background-position","20% " + ($(this).scrollTop() / 2) + "px");
+});
+});
+
